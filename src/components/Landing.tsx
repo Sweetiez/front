3e8f6750beb8e614cba3cdc24446fad7eca35949
Gwendal <gwendou95@hotmail.fr>
2022-03-23 00:41:26 +0100

    feat: init work on the shop

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -1,10 +1,12 @@
 import Page from './Page/Page';
 import NavMenu from './NavMenu/NavMenu';
+import Shop from './Shop/Shop';
 
 const Landing: React.FC = () => {
   return (
     <Page>
       <NavMenu />
+      <Shop />
     </Page>
   );
 };
