783818daedf9becc6fc9a8e7ab083879b01ee666
Gwendal <gwendou95@hotmail.fr>
2022-03-23 01:21:58 +0100

    feat: product card rendering

---
+++ src/components/Shop/ProductCardModel.ts
@@ -0,0 +1,6 @@
+export default class ProductCardModel {
+    id: string | undefined;
+    name: string | undefined;
+    price: number | undefined;
+    image: string | undefined;
+}
--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
 import ProductCard from './ProductCard';
+import {fakeProducts} from "../../assets/FakeProducts";
 
 const Shop: React.FC = () => {
+    const products = fakeProducts;
   return (
-    <div className="h-screen">
+    <div className="">
       <div className="grid grid-cols-6">
         <div className="col-start-2 col-end-6">
-          <div className="grid grid-cols-4">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
+          <div className="grid grid-cols-4 grid-rows-2">
+            {products.map((product) => (
+              <ProductCard key={product.id} product={product} />
+            ))}
           </div>
         </div>
         <div />
