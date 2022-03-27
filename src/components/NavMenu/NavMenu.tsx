965c620668f3bc41f5e2a8f4488451df9f9f118a
Gwendal <gwendou95@hotmail.fr>
2022-03-27 16:05:53 +0200

    feat(Cart): side cart modal

--- src/components/NavMenu/NavMenu.tsx
+++ src/components/NavMenu/NavMenu.tsx
@@ -1,8 +1,9 @@
 import { Dialog, Popover, Tab, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { MenuIcon, ShoppingBagIcon, XIcon } from '@heroicons/react/outline';
 import Lottie from 'react-lottie-player';
 import cakeAnimation from '../../assets/cakerun.json';
+import CartModal from '../Cart/CartModal';
 
 const navigation = {
   categories: [
@@ -140,6 +141,7 @@ function classNames(...classes: any[]) {
 
 const NavMenu: React.FC = () => {
   const [open, setOpen] = useState(false);
+  const [cartOpen, setCartOpen] = useState(false);
 
   return (
     <>
@@ -448,7 +450,10 @@ const NavMenu: React.FC = () => {
 
                   {/* Cart */}
                   <div className="ml-4 flow-root lg:ml-6">
-                    <div className="group -m-2 p-2 flex items-center">
+                    <div
+                      onClick={() => setCartOpen(true)}
+                      className="group -m-2 p-2 flex items-center"
+                    >
                       <ShoppingBagIcon
                         className="flex-shrink-0 h-6 w-6 text-gray-400 group-hover:text-gray-500"
                         aria-hidden="true"
@@ -465,6 +470,45 @@ const NavMenu: React.FC = () => {
           </nav>
         </header>
       </div>
+      <Transition.Root show={cartOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="z-50 fixed inset-0 overflow-hidden"
+          onClose={setCartOpen}
+        >
+          <div className="absolute inset-0 overflow-hidden">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-in-out duration-500"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="ease-in-out duration-500"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <Dialog.Overlay className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            </Transition.Child>
+
+            <div className="pointer-events-none fixed inset-y-0 right-0 flex max-w-full pl-10">
+              <Transition.Child
+                as={Fragment}
+                enter="transform transition ease-in-out duration-500 sm:duration-700"
+                enterFrom="translate-x-full"
+                enterTo="translate-x-0"
+                leave="transform transition ease-in-out duration-500 sm:duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+              >
+                <div className="pointer-events-auto w-screen max-w-md">
+                  <div className="flex h-full flex-col overflow-y-scroll bg-white shadow-xl">
+                    <CartModal setOpenedModal={setCartOpen} />
+                  </div>
+                </div>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 };
