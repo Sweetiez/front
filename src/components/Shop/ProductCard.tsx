b90618f4f6bd3ec7a8fec4342d31c59c04a94e20
LAymeric <aymeric.larvet@gmail.com>
2022-04-03 13:03:05 +0200

    refactor: current product is managed and modal opens only once

--- src/components/Shop/ProductCard.tsx
+++ src/components/Shop/ProductCard.tsx
@@ -1,23 +1,19 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import ProductCardModel from './ProductCardModel';
 import Stars from '../Stars/Stars';
-import { Transition, Dialog } from '@headlessui/react';
-import QuickShop from './QuickShop';
 import Lottie from 'react-lottie-player';
 import cartAnimation from '../../assets/cart.json';
 
 interface ProductCardProps {
   product: ProductCardModel;
-  onImageClick: () => void;
-  setModalState: (state: boolean) => void;
-  modalState: boolean;
+  onImageClick: (product: ProductCardModel) => void;
+  openBasket: (product: ProductCardModel, state: boolean) => void;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
   product,
   onImageClick,
-  modalState,
-  setModalState,
+  openBasket,
 }) => {
   const [isCartHover, setIsCartHover] = useState(false);
 
@@ -30,13 +26,14 @@ const ProductCard: React.FC<ProductCardProps> = ({
               className="h-40 rounded-2xl w-full object-cover"
               src="https://pixahive.com/wp-content/uploads/2020/10/Gym-shoes-153180-pixahive.jpg"
               alt="product-item"
-              onClick={onImageClick}
+              onClick={() => onImageClick(product)}
             />
             <div
               onMouseEnter={() => setIsCartHover(true)}
               onMouseLeave={() => setIsCartHover(false)}
               onClick={() => {
-                setModalState(true);
+                console.log('test');
+                openBasket(product, true);
               }}
               className="absolute right-2 top-2 bg-white rounded-full p-2 cursor-pointer group"
             >
@@ -62,47 +59,6 @@ const ProductCard: React.FC<ProductCardProps> = ({
           </div>
         </div>
       </div>
-
-      <Transition.Root show={modalState} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed z-10 inset-0 overflow-y-auto"
-          onClose={setModalState}
-        >
-          <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-            </Transition.Child>
-            <span
-              className="hidden sm:inline-block sm:align-middle sm:h-screen"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            >
-              <div className=" inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-                <QuickShop product={product} setOpenedModal={setModalState} />
-              </div>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition.Root>
     </>
   );
 };
