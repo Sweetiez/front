7389460db3c63c40c55306b527b25e6eed26dae1
LAymeric <aymeric.larvet@gmail.com>
2022-04-04 17:36:03 +0200

    feat: change animation of carousel

--- src/components/Shop/Shop.tsx
+++ src/components/Shop/Shop.tsx
@@ -7,6 +7,10 @@ import QuickShop from './QuickShop';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import '../../assets/css/_carousel.css';
+import {
+  AnimationHandler,
+  AnimationHandlerResponse,
+} from 'react-responsive-carousel/lib/ts/components/Carousel/types';
 
 const Shop: React.FC = () => {
   const products = fakeProducts;
@@ -29,6 +33,47 @@ const Shop: React.FC = () => {
     setModalState(true);
   }, []);
 
+  const fadeAnimationHandler: AnimationHandler = (
+    props,
+    state,
+  ): AnimationHandlerResponse => {
+    const transitionTime = props.transitionTime + 'ms';
+    const transitionTimingFunction = 'ease-in-out';
+
+    let slideStyle: React.CSSProperties = {
+      position: 'absolute',
+      display: 'block',
+      minHeight: '100%',
+      opacity: 0,
+      top: 0,
+      right: 0,
+      left: 0,
+      bottom: 0,
+      transitionTimingFunction: transitionTimingFunction,
+      msTransitionTimingFunction: transitionTimingFunction,
+      MozTransitionTimingFunction: transitionTimingFunction,
+      WebkitTransitionTimingFunction: transitionTimingFunction,
+      OTransitionTimingFunction: transitionTimingFunction,
+    };
+
+    if (!state.swiping) {
+      slideStyle = {
+        ...slideStyle,
+        WebkitTransitionDuration: transitionTime,
+        MozTransitionDuration: transitionTime,
+        OTransitionDuration: transitionTime,
+        transitionDuration: transitionTime,
+        msTransitionDuration: transitionTime,
+      };
+    }
+
+    return {
+      slideStyle,
+      selectedStyle: { ...slideStyle, opacity: 1, position: 'relative' },
+      prevStyle: { ...slideStyle },
+    };
+  };
+
   return (
     <>
       <div className="py-8 container w-4/5 mx-auto">
@@ -47,6 +92,8 @@ const Shop: React.FC = () => {
                 showStatus={false}
                 autoPlay={true}
                 infiniteLoop={true}
+                swipeable={false}
+                animationHandler={fadeAnimationHandler}
                 interval={3000}
                 className="rounded-3xl overflow-hidden"
               >
