d85d1f9f65ded0458ae1af04dd4dbe373361f6d7
Gwendal <gwendou95@hotmail.fr>
2022-04-20 00:17:02 +0200

    feat(checkout): cart payment

--- src/components/Checkout/CheckoutPayment.tsx
+++ src/components/Checkout/CheckoutPayment.tsx
@@ -1,70 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { DateTime } from 'luxon';
 
 const CheckoutPayment: React.FC = () => {
+  const [cardSide, setCardSide] = useState(true);
+  const [cardNumber, setCardNumber] = useState('0000 0000 0000 0000');
+  const [cardholder, setCardholder] = useState('Your Name');
+  const [expiredMonth, setExpiredMonth] = useState('MM');
+  const [expiredYear, setExpiredYear] = useState('YY');
+  const [securityCode, setSecurityCode] = useState('');
+  const cardYears = [];
+  // Just to not write the same things over and over again
+  const cardMonths = [
+    '01',
+    '02',
+    '03',
+    '04',
+    '05',
+    '06',
+    '07',
+    '08',
+    '09',
+    '10',
+    '11',
+    '12',
+  ];
+
+  // Initialize card years expiration (may be changed in the future)
+  for (let i = 0; i < 6; i++) {
+    cardYears.push(DateTime.local().year + i);
+  }
+
+  function handleCardNumerChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    setCardNumber(value);
+  }
+
   return (
-    <>
-      <div className="m-4">
+    <div className="grid grid-cols-6">
+      <div className="col-start-3 col-end-5 xs:col-start-1 xs:col-end-7">
         <div
-          className="credit-card w-full sm:w-auto shadow-lg mx-auto rounded-xl bg-white"
+          className="credit-card w-full shadow-lg mx-auto rounded-xl bg-white flex flex-col"
           x-data="creditCard"
         >
           <header className="flex flex-col justify-center items-center">
-            <div className="relative">
-              <img
-                className="w-full h-auto"
-                src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
-                alt="front credit card"
-              />
-              <div className="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-12">
-                <p
-                  className="number mb-5 sm:text-xl"
-                  x-text="cardNumber !== '' ? cardNumber : '0000 0000 0000 0000'"
-                ></p>
-                <div className="flex flex-row justify-between">
-                  <p x-text="cardholder !== '' ? cardholder : 'Card holder'"></p>
-                  <div className="">
-                    <span x-text="expired.month"></span>
-                    <span x-show="expired.month !== ''">/</span>
-                    <span x-text="expired.year" />
+            {cardSide ? (
+              <div className="relative">
+                <img
+                  className="w-full h-auto"
+                  src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-front.png"
+                  alt="front credit card"
+                />
+                <div className="front bg-transparent text-lg w-full text-white px-12 absolute left-0 bottom-24">
+                  <p className="number mb-5 sm:text-xl">
+                    {formatCardNumber(cardNumber)}
+                  </p>
+                  <div className="flex flex-row justify-between">
+                    <p>{cardholder}</p>
+                    <div className="">
+                      <span x-text="expired.month">{expiredMonth}</span>
+                      <span x-show="expired.month !== ''">/</span>
+                      <span x-text="expired.year">{expiredYear}</span>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="relative">
-              <img
-                className="w-full h-auto"
-                src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-back.png"
-                alt=""
-              />
-              <div className="bg-transparent text-white text-xl w-full flex justify-end absolute bottom-20 px-8  sm:bottom-24 right-0 sm:px-12">
-                <div className="border border-white w-16 h-9 flex justify-center items-center">
-                  <p x-text="securityCode !== '' ? securityCode : 'code'"></p>
+            ) : (
+              <div className="relative">
+                <img
+                  className="w-full h-auto"
+                  src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/svg-cards/card-visa-back.png"
+                  alt=""
+                />
+                <div className="bg-transparent text-white text-xl w-full flex justify-end absolute bottom-20 px-8  sm:bottom-24 right-0 sm:px-12">
+                  <div className="border border-white w-16 h-9 flex justify-center items-center">
+                    <p>{securityCode}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <ul className="flex">
-              <li className="mx-2">
-                <img
-                  className="w-16"
-                  src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/computop.png"
-                  alt=""
-                />
-              </li>
-              <li className="mx-2">
-                <img
-                  className="w-14"
-                  src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/verified-by-visa.png"
-                  alt=""
-                />
-              </li>
-              <li className="ml-5">
-                <img
-                  className="w-7"
-                  src="https://www.computop-paygate.com/Templates/imagesaboutYou_desktop/images/mastercard-id-check.png"
-                  alt=""
-                />
-              </li>
-            </ul>
+            )}
           </header>
           <main className="mt-4 p-4">
             <h1 className="text-xl font-semibold text-gray-700 text-center">
@@ -77,13 +90,18 @@ const CheckoutPayment: React.FC = () => {
                   className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                   placeholder="Card holder"
                   x-model="cardholder"
+                  onSelect={() => setCardSide(true)}
+                  onChange={(e) => setCardholder(e.target.value)}
                 />
               </div>
               <div className="my-3">
                 <input
-                  type="text"
+                  type="tel"
                   className="block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                   placeholder="Card number"
+                  onSelect={() => setCardSide(true)}
+                  maxLength={16}
+                  onChange={(e) => handleCardNumerChange(e)}
                 />
               </div>
               <div className="my-3 flex flex-col">
@@ -96,42 +114,40 @@ const CheckoutPayment: React.FC = () => {
                     id=""
                     className="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                     x-model="expired.month"
+                    onChange={(e) => setExpiredMonth(e.target.value)}
                   >
-                    <option value="" selected disabled>
+                    <option value="" disabled>
                       MM
                     </option>
-                    <option value="01">01</option>
-                    <option value="02">02</option>
-                    <option value="03">03</option>
-                    <option value="04">04</option>
-                    <option value="05">05</option>
-                    <option value="06">06</option>
-                    <option value="07">07</option>
-                    <option value="08">08</option>
-                    <option value="09">09</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
+                    {cardMonths.map((month, index) => (
+                      <option key={index} value={month}>
+                        {month}
+                      </option>
+                    ))}
                   </select>
                   <select
                     name=""
                     id=""
                     className="form-select appearance-none block w-full px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
+                    onChange={(e) => setExpiredYear(e.target.value)}
+                    defaultValue="YY"
                   >
-                    <option value="" selected disabled>
+                    <option value="" disabled>
                       YY
                     </option>
-                    <option value="2021">2021</option>
-                    <option value="2022">2022</option>
-                    <option value="2023">2023</option>
-                    <option value="2024">2024</option>
-                    <option value="2025">2025</option>
-                    <option value="2026">2026</option>
+                    {cardYears.map((year) => (
+                      <option key={year} value={year}>
+                        {year}
+                      </option>
+                    ))}
                   </select>
                   <input
-                    type="text"
+                    type="tel"
                     className="block w-full col-span-2 px-5 py-2 border rounded-lg bg-white shadow-lg placeholder-gray-400 text-gray-700 focus:ring focus:outline-none"
                     placeholder="Security code"
+                    onSelect={() => setCardSide(false)}
+                    onChange={(e) => setSecurityCode(e.target.value)}
+                    maxLength={3}
                   />
                 </div>
               </div>
@@ -144,8 +160,33 @@ const CheckoutPayment: React.FC = () => {
           </footer>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
 export default CheckoutPayment;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function verifyCardNumber(cardNumber: string) {
+  const regex = /^[0-9]{4}[0-9]{4}[0-9]{4}[0-9]{4}$/;
+  return regex.test(cardNumber);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function verifySecurityCode(securityCode: string) {
+  const regex = /^[0-9]{3}$/;
+  return regex.test(securityCode);
+}
+
+function formatCardNumber(cardNumber: string) {
+  cardNumber = cardNumber.replace(/\s/g, '');
+  return (
+    cardNumber.substring(0, 4) +
+    ' ' +
+    cardNumber.substring(4, 8) +
+    ' ' +
+    cardNumber.substring(8, 12) +
+    ' ' +
+    cardNumber.substring(12, 16)
+  );
+}
