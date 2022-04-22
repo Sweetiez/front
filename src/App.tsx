40c37c6bb1fca7eb8b9e19be832a31058f22d336
Gwendal <gwendou95@hotmail.fr>
2022-04-22 23:19:56 +0200

    feat(recipes): navigation on recipes and events

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Landing from './components/Landing';
 import Checkout from "./components/Checkout/Checkout";
+import Recipes from "./components/Recipes/Recipes";
 
 function App() {
   const queryClient = new QueryClient();
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Landing />} />
             <Route path="/checkout" element={<Checkout />} />
+            <Route path="/recipes" element={<Recipes />} />
           </Routes>
         </Router>
       </QueryClientProvider>
