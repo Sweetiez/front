1cd9166151ed32ef1b2f948f7753c6f8cdc65a59
Gwendal <gwendou95@hotmail.fr>
2022-04-23 00:23:11 +0200

    feat(recipes): recipes page

---
+++ src/components/Recipes/RecipeModel.ts
@@ -0,0 +1,7 @@
+export default class RecipeModel {
+  id: string | undefined;
+  name: string | undefined;
+  description: string | undefined;
+  image: string | undefined;
+  rating: number | undefined;
+}
