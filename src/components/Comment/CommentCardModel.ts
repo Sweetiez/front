f0dd1fcae25e5dc4b4f93443b0f7a0d00558a83f
LAymeric <aymeric.larvet@gmail.com>
2022-05-26 18:18:24 +0200

    feat(font): adding name of comment author

--- src/components/Comment/CommentCardModel.ts
+++ src/components/Comment/CommentCardModel.ts
@@ -1,6 +1,7 @@
 export default class CommentCardModel {
   id: string | undefined;
   author: string | undefined;
+  authorName: string | undefined;
   content: string | undefined;
   rating: number | undefined;
   date: string | undefined;
