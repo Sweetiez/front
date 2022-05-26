027da6b649066778d89f52c698b01b2ea21b61a2
LAymeric <aymeric.larvet@gmail.com>
2022-05-26 20:22:50 +0200

    feat(font): fix reason of report other

--- src/hooks/evaluations/requests/reportEvaluationRequest.ts
+++ src/hooks/evaluations/requests/reportEvaluationRequest.ts
@@ -2,17 +2,17 @@ export default class ReportEvaluationRequest {
   reason: number | undefined;
   reporterId: string | undefined;
   evaluationId: string | undefined;
-  // content: string | undefined;
+  content: string | undefined;
 
   constructor(
     reason: number | undefined,
     reporterId: string | undefined,
     evaluationId: string | undefined,
-    // content: string | undefined,
+    content: string | undefined,
   ) {
     this.reason = reason;
     this.reporterId = reporterId;
     this.evaluationId = evaluationId;
-    // this.content = content;
+    this.content = content;
   }
 }
