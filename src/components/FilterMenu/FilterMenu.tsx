3bc935725894b9abbe060295948e7dd5ca2454ed
LAymeric <aymeric.larvet@gmail.com>
2022-05-28 15:28:41 +0200

    refactor(front): design of filter menu

--- src/components/FilterMenu/FilterMenu.tsx
+++ src/components/FilterMenu/FilterMenu.tsx
@@ -1,53 +1,61 @@
-import React from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useState } from 'react';
 
 import Lottie from 'react-lottie-player';
 import filter from '../../assets/lotties/filter.json';
 import '../../assets/css/_filter-menu.css';
-import TextButton from "../Authentication/TextButton";
 
 const FilterMenu = () => {
-  const { t } = useTranslation();
+  const [activeFilter, setActiveFilter] = useState(1);
+
+  const navigationOptions = [
+    {
+      index: 1,
+      option: 'Mignardises',
+    },
+    {
+      index: 2,
+      option: 'Plateaux',
+    },
+  ];
+
+  function setActiveFilterMenu(index: number) {
+    setActiveFilter(index);
+  }
+
   return (
     <>
-        <nav className="bg-white shadow w-full p-1">
-            <ul className="flex justify-center items-center">
-                <li className="active">
-                    <button
-                        className="background-transparent outline-none hover:text-gold-100 focus:outline-none nav-link nav-link-fade-up font-pompiere text-xl"
-                        type="button"
-                    >
-                        Mignardises
-                    </button>
+      <nav className="bg-white shadow w-full p-1">
+        <ul className="flex justify-center items-center">
+          {navigationOptions &&
+            navigationOptions.map(({ index, option }) => {
+              return (
+                <li key={option}>
+                  <button
+                    className={`${
+                      activeFilter === index
+                        ? 'active'
+                        : 'nav-link nav-link-ltr'
+                    } background-transparent font-semibold outline-none hover:text-gold-100 focus:outline-none font-pompiere text-xl`}
+                    type="button"
+                    onClick={() => setActiveFilterMenu(index)}
+                  >
+                    {option}
+                  </button>
                 </li>
-                <li className="">
-                    <button
-                        className="background-transparent outline-none hover:text-gold-100 focus:outline-none nav-link nav-link-fade-up font-pompiere text-xl"
-                        type="button"
-                    >
-                        Plateaux
-                    </button>
-                </li>
-                <div className="border-left-black h-full w-1"/>
-                {/*<button*/}
-                {/*    className="grid grid-flow-col auto-cols-max background-transparent ml-1 outline-none hover:text-gold-100 focus:outline-none nav-link nav-link-fade-up font-pompiere text-xl"*/}
-                {/*    type="button"*/}
-                {/*>*/}
-                {/*    <Lottie className="h-4 w-auto" loop animationData={filter} play />*/}
-                {/*    <span className="ml-2 text-xs content-center">Filters</span>*/}
-                {/*</button>*/}
-                {/*<div className="border-l border-black pl-2">*/}
-                <div className="divider"/>
-                    <button
-                        className="ease-linear transform transition-all duration-150 hover:scale-105 ml-3.5 grid grid-flow-col auto-cols-max outline-none r-0 items-center text-black font-bold text-xs pl-3 py-1 focus:outline-none mr-2 mb-1"
-                        type="button"
-                    >
-                        <Lottie className="h-4 w-auto" loop animationData={filter} play />
-                        <span className="ml-2 text-xs content-center font-pompiere text-xl">More Filters</span>
-                    </button>
-                {/*</div>*/}
-            </ul>
-        </nav>
+              );
+            })}
+          <div className="divider" />
+          <button
+            className="ease-linear transform transition-all font-semibold duration-300 hover:scale-105 ml-3.5 grid grid-flow-col auto-cols-max outline-none r-0 items-center text-black text-xs pl-3 py-1 focus:outline-none mr-2 mb-1"
+            type="button"
+          >
+            <Lottie className="h-4 w-auto" loop animationData={filter} play />
+            <span className="ml-2 text-xs content-center font-pompiere text-xl">
+              More Filters
+            </span>
+          </button>
+        </ul>
+      </nav>
     </>
   );
 };
