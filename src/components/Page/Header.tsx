e58c325261b728270f8b15a33f33f7d965312b63
LAymeric <aymeric.larvet@gmail.com>
2022-06-01 21:29:24 +0200

    fix(font): click on logo redirect to shop

--- src/components/Page/Header.tsx
+++ src/components/Page/Header.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
   return (
     <>
       <div className="sticky top-0 z-40 min-w-full h-8 container bg-beige-100 shadow">
         <div className="grid grid-cols-6 justify-between items-center h-full">
-          <div />
           <div className="invisible md:visible lg:visible xl:visible z-50 col-start-4 ">
-            <img
-              className="object-cover -ml-16 w-32 h-32 rounded-full shadow"
-              src="https://media.discordapp.net/attachments/548183152376545306/955953631805452349/logo.jpg"
-              alt="logo"
-            />
+            <Link to={'/'}>
+              <img
+                className="object-cover -ml-16 w-32 h-32 rounded-full shadow"
+                src="https://media.discordapp.net/attachments/548183152376545306/955953631805452349/logo.jpg"
+                alt="logo"
+              />
+            </Link>
           </div>
         </div>
       </div>
