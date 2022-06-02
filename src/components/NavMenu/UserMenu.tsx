574cbbcb1cfda8c4a3529f097b21536aa2e6cfca
LAymeric <aymeric.larvet@gmail.com>
2022-06-02 20:55:07 +0200

    refactor(font): adding active class on navigation menu + badge on cart + fix with of user menu

--- src/components/NavMenu/UserMenu.tsx
+++ src/components/NavMenu/UserMenu.tsx
@@ -19,7 +19,7 @@ const UserMenu = () => {
       </span>
       <div className="group inline-block">
         <button className="outline-none focus:outline-none px-3 py-1 bg-white rounded-sm flex items-center min-w-32">
-          <span className="font-medium text-gray-900 font-birthstone text-2xl p-2 block">
+          <span className="font-medium font-birthstone text-2xl p-2 block">
             {profile?.name}
           </span>
           <span>
@@ -32,7 +32,7 @@ const UserMenu = () => {
             </svg>
           </span>
         </button>
-        <ul className="bg-white border rounded-lg transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32">
+        <ul className="bg-white border rounded-lg transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top">
           <button>
             <Link to="/user/profile">
               <li className="rounded-sm px-3 py-1 hover:bg-gray-100 font-birthstone text-2xl">
