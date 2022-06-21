d12ef968438588a7146dc0cb1fa695910d81aa68
chukitipok <antonin.texier@gmail.com>
2022-06-21 02:59:35 +0200

    fix(app): fix footer for all browsers

--- src/components/Page/Footer.tsx
+++ src/components/Page/Footer.tsx
@@ -4,7 +4,7 @@ import Socials from "../Contacts/Socials";
 const Footer: React.FC = () => {
   return (
     <footer className="text-center bg-gray-900 text-white">
-      <div className="justify-items-center pt-6">
+      <div className="grid place-items-center justify-items-center pt-6">
         <Socials color={""}/>
       </div>
 
