943447690081812a4999593f8b150aeb4c084acc
LAymeric <aymeric.larvet@gmail.com>
2022-06-28 23:01:42 +0200

    feat(front): change locale

--- src/components/Page/Footer.tsx
+++ src/components/Page/Footer.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Socials from "../Contacts/Socials";
-import frenchFlag from './../../assets/icons/frenchFlag.png'
-import englishFlag from './../../assets/icons/englishFlag.png'
-import i18n from "i18next";
 
 const Footer: React.FC = () => {
   return (
@@ -10,12 +7,6 @@ const Footer: React.FC = () => {
       <div className="grid place-items-center justify-items-center pt-6">
         <Socials color={""}/>
       </div>
-        <div className="flex justify-center mt-3">
-            <img src={frenchFlag} alt="French Flag" style={{width:'25px', height:'15px'}} className="mr-2"
-            onClick={() => i18n.changeLanguage('fr-FR')}/>
-            <img src={englishFlag} alt="English Flag" style={{width:'25px', height:'15px'}}
-                 onClick={() => i18n.changeLanguage('en-EN')}/>
-        </div>
       <div className="justify-items-center p-4">© 2022 - FI-Sweets</div>
     </footer>
   );
