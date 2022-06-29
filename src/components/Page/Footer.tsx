f7e14e58a2f07e6a2797abf6dcc6cf42bcda2cb1
Gwendal <gwendou95@hotmail.fr>
2022-06-29 20:06:18 +0200

    fix(footer): socials and privacy link

--- src/components/Page/Footer.tsx
+++ src/components/Page/Footer.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import Socials from "../Contacts/Socials";
+import Socials from '../Contacts/Socials';
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 const Footer: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <footer className="text-center bg-gray-900 text-white">
       <div className="grid place-items-center justify-items-center pt-6">
-        <Socials color={""}/>
+        <div className="flex">
+          <Link to="/privacy" className="p-2 hover:underline">
+            {t('footer.privacy')}
+          </Link>
+          <Socials color={''} />
+          <Link to="/privacy" className="p-2 hover:underline">
+            {t('footer.terms')}
+          </Link>
+        </div>
       </div>
       <div className="justify-items-center p-4">© 2022 - FI-Sweets</div>
     </footer>
